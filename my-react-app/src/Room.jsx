4ffc8bcc450ebb15b174e5c74c4da9b6486c0c94
AJasem <ajassem.ahmad@gmail.com>
2024-08-04 04:26:25 +0200

    mobile

--- my-react-app/src/Room.jsx
+++ my-react-app/src/Room.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './Room.css';
 import { useRoomContext } from './RoomContext.jsx';
@@ -10,15 +10,14 @@ function Room() {
   const { roomName, setRoomName, userName, setUserName, messages, setMessages } = useRoomContext();
   const [newMessage, setNewMessage] = useState('');
   const [socket, setSocket] = useState(null);
+  const messagesEndRef = useRef(null);
 
   useEffect(() => {
-
     const socket = io('https://socketio.ahmads.dev');  
     setSocket(socket);
 
     socket.on('connect', () => {
       console.log('Connected to Socket.IO server');
-     
       socket.emit('join', { room: roomName, user: userName });
     });
 
@@ -36,6 +35,12 @@ function Room() {
     return () => socket.disconnect();
   }, [roomName, userName]);
 
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [messages]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     if (socket) {
@@ -48,16 +53,13 @@ function Room() {
     }
   };
 
-  const handleRefresh = (e1,e2) => {
-    const formData = {
-      roomName: e1,
-      userName: e2,
-    };
+  const handleRefresh = (e1, e2) => {
+    const formData = { roomName: e1, userName: e2 };
 
     axios.post('https://chatapi.ahmads.dev/api/checkroom', formData)
       .then(response => {
         const messages = response.data.messages || [];
-        setMessages(messages.map(msg => ({ text: msg.value, sender: msg.user, date: msg.date})));
+        setMessages(messages.map(msg => ({ text: msg.value, sender: msg.user, date: msg.date })));
       })
       .catch(error => {
         console.log('Error starting room:', error);
@@ -70,7 +72,7 @@ function Room() {
 
     if (storedRoomName) setRoomName(storedRoomName);
     if (storedUserName) setUserName(storedUserName);
-    handleRefresh(storedRoomName,storedUserName);
+    handleRefresh(storedRoomName, storedUserName);
   }, [roomName]);
 
   const handleExit = () => {
@@ -94,9 +96,9 @@ function Room() {
               <span>{message.sender}</span>
               <br />
               <p>{message.text}</p>
-             
             </div>
           ))}
+          <div ref={messagesEndRef} />
         </div>
         <div className='send-msg'>
           <form onSubmit={handleSubmit}>
@@ -106,6 +108,7 @@ function Room() {
               onChange={(e) => setNewMessage(e.target.value)}
               className='msg'
               required
+              placeholder='Write a message'
             />
             <button type='submit' className='send'>Send</button>
           </form>
